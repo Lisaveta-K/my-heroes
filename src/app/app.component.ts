be5c4f3d5dc5f849165b960866bacad69f4c0d85
Elizaveta Khudiakova <lisavaeta.khudiakova@gmail.com>
2017-04-24 22:26:55 +0800

    new functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,28 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Here we go, some superheroes for humanity ;)';
+  heroes = HEROES;  
+  selectedHero: Hero;
 
-  hero: Hero = {
-	  id: 1,
-	  name: 'Batman',
-	  age: 35
-	};
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero {
   id: number;
   name: string;
-  age: number;
 }
+
+const HEROES: Hero[] = [
+{ id:1, name: 'Black Widow' },
+{ id:2, name: 'Spiderman' },
+{ id:3, name: 'Ninja Tortle' },
+{ id:4, name: 'Flash' },
+{ id:5, name: 'Jessica Jones' },
+{ id:6, name: 'Tor' },
+{ id:7, name: 'Captain Hammer' },
+{ id:8, name: 'The Doctor' },
+{ id:9, name: 'Cat Lady' },
+{ id:10, name: 'Jenny the Mechanik' }
+];
